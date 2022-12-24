6cb542bb58eb2db549695ecf776a481701fc6557
Dan <pintea.dan988@gmail.com>
2022-12-24 12:50:04 +0100

    Implemented the frontend/backend.

---
+++ client/script.js
@@ -0,0 +1,139 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+/* Load the messages */
+let loadInterval = null;
+
+/* Will show 3 dynamic dots as an indication for fetching info
+   from OpenAI API's */
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+/* Will animate typing text */
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+/* Generate unique IDs for each message div */
+function generateUniqueId() {
+    // Used to generate unique IDs
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+/* Generates the message log and displays it to the user */
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+/*  */
+const handleSubmit = async (e) => {
+    // Will prevent reloading of the browser
+    e.preventDefault()
+
+    // Data from the form
+    const data = new FormData(form)
+
+    // User's message chatStripe()
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // Will clear the textarea input
+    form.reset()
+
+    // Bot's message chatStripe()
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // Will focus scroll to the bottom
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // Specific message div
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        const data = await response.json()
+        // Trims any trailing spaces/'\n'
+        const parsedData = data.bot.trim()
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const error = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(error)
+    }
+}
+
+form.addEventListener('submit', handleSubmit)
+// Will send the input if "Enter" is pressed
+form.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        handleSubmit(e)
+    }
+})
+
+// Make the textarea focused on each page reload
+window.onload = function () {
+    const text = document.getElementById('textarea-id');
+    text.focus()
+
+    window.onhashchange = function () {
+        text.focus()
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,55 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import {Configuration, OpenAIApi} from 'openai'
+
+dotenv.config()
+
+// Environment configuration
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Will send this message to the server's user
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from ChatGPT Codex!'
+    })
+})
+
+// Will generate the payload of the AI bot
+app.post('/', async (req, res) => {
+    try {
+        // Will prompt the message from the frontend. Needed for OpenAI API
+        const prompt = req.body.prompt;
+
+        // Fetch a response prom OpenAI API
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0, // Higher values means the model will take more risks.
+            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+            top_p: 1, // Alternative to sampling with temperature, called nucleus sampling
+            frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        });
+        console.log(response)
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error || 'Something went wrong');
+    }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
