570a265b2284772fb057889db672baa8f499ea6f
Dan <pintea.dan988@gmail.com>
2022-12-25 20:42:30 +0100

    Bug fixes and some UI improvements.

--- client/script.js
+++ client/script.js
@@ -93,7 +93,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('http://localhost:5001/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -115,8 +115,8 @@ const handleSubmit = async (e) => {
     } else {
         const error = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(error)
+        messageDiv.innerHTML = error.toString()
+        // alert(error)
     }
 }
 
@@ -137,3 +137,4 @@ window.onload = function () {
         text.focus()
     }
 }
+
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import {Configuration, OpenAIApi} from 'openai'
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
 // Environment configuration
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -20,7 +20,7 @@ app.use(express.json())
 // Will send this message to the server's user
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello from ChatGPT Codex!'
+        message: 'Hello from CodeX!'
     })
 })
 
@@ -34,13 +34,12 @@ app.post('/', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0, // Higher values means the model will take more risks.
+            temperature: 0.7, // Higher values means the model will take more risks.
             max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-            top_p: 1, // Alternative to sampling with temperature, called nucleus sampling
+            top_p: 1.0, // Alternative to sampling with temperature, called nucleus sampling
             frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
             presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
         });
-        console.log(response)
 
         res.status(200).send({
             bot: response.data.choices[0].text
@@ -52,4 +51,4 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5001, () => console.log('AI server started on http://localhost:5001'))
